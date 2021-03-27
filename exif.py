7be42320137a22f71e870bf6a8ea9cc8028cde1f
Dennis Vashchuk <dennis.vashchuk@gmail.com>
2021-03-27 19:25:09 +0000

    Ignore photos without geo info

--- exif.py
+++ exif.py
@@ -5,5 +5,7 @@ def get_geo_info(file_path):
     # Get the data from image file and return a dictionary
     data = gpsphoto.getGPSData(file_path)
     rawData = gpsphoto.getRawData(file_path)
-    return (data['Latitude'], data['Longitude'])
-
+    if 'Latitue' in data:
+        return (data['Latitude'], data['Longitude'])
+    else:
+        return None
--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import geocoding
 
 for file_path in sys.argv[1:]:
     coordinates = exif.get_geo_info(file_path)
-    location = geocoding.rgeocode(coordinates)
-    country, state, city = location.raw['address']['country'], location.raw['address']['state'], location.raw['address']['city']
-    image.add_text(file_path, f"{city}, {state}, {country}")
+    if coordinates:
+        location = geocoding.rgeocode(coordinates)
+        country, state, city = location.raw['address']['country'], location.raw['address']['state'], location.raw['address']['city']
+        image.add_text(file_path, f"{city}, {state}, {country}")
+    else:
+        image.add_text(file_path, f"")
